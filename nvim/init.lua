08a3e61ed6b138b0e419974833e143622dcd8fda
Harry Llewelyn <harry@hey.com>
2022-03-31 14:57:07 +0100

    Dont need to set this twice

--- nvim/init.lua
+++ nvim/init.lua
@@ -96,7 +96,6 @@ vim.cmd [[
 
   let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
   let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-  set termguicolors
 
   " Shortcut commands to change the colorscheme - mainly used for pairing
   command! DD execute "colorscheme onehalfdark"
