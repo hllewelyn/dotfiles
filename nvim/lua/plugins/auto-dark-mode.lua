888d3c0b662247619913046820cff0b347c24cc3
Harry Llewelyn <harry@hey.com>
2025-01-09 09:59:50 +0000

    Switch Ghostty and Neovim theme to catppuccin

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -1,14 +1,16 @@
 return {
-  "f-person/auto-dark-mode.nvim",
-  opts = {
-    update_interval = 1000,
-    set_dark_mode = function()
-      vim.api.nvim_set_option_value("background", "dark", {})
-      vim.cmd('colorscheme github_dark')
-    end,
-    set_light_mode = function()
-      vim.api.nvim_set_option_value("background", "light", {})
-      vim.cmd("colorscheme github_light")
-    end,
-  },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd('colorscheme catppuccin-frappe')
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme catppuccin-latte")
+      end,
+    },
+  }
 }
--- nvim/lua/plugins/github-nvim-theme.lua
+++ nvim/lua/plugins/github-nvim-theme.lua
@@ -2,6 +2,7 @@ return {
   {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
+    enabled = false,
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
@@ -11,20 +12,6 @@ return {
         }
       })
 
-      vim.cmd('syntax enable')
-      vim.cmd('colorscheme github_light')
-      vim.cmd [[
-        " Change the color of the editor after 120 chars, except for the quickfix panel.
-        let &colorcolumn=join(range(121,999),",")
-        au FileType qf setlocal nonumber colorcolumn=
-
-        let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
-        let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-
-        " Shortcut commands to change the colorscheme - mainly used for pairing
-        command! DD execute "colorscheme github_dark"
-        command! DL execute "colorscheme github_light"
-      ]]
     end,
   }
 }
