6d8c32ef710a47b0ca90620ee012a4ee5ee2e6cb
Harry Llewelyn <harry@hey.com>
2025-09-25 08:58:45 +0100

    Formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -106,7 +106,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    vim.opt_local.foldmethod = 'expr'
-    vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.opt_local.foldmethod = "expr"
+    vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
   end,
 })
