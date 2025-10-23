aea463633eb279c35f9451da362413bce87ffef2
Harry Llewelyn <harry@hey.com>
2025-10-23 14:18:39 +0100

    Add and configure nvim-treesitter-textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   config = function ()
     local configs = require("nvim-treesitter.configs")
 
@@ -11,6 +14,22 @@ return {
       highlight = { enable = true },
       indent = { enable = true },
       additional_vim_regex_highlighting = true,
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["am"] = "@function.outer",
+            ["im"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ab"] = "@block.outer",
+            ["ib"] = "@block.inner",
+          },
+        },
+      },
     })
   end
 }
