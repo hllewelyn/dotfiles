4f351f39494a90b65be31c229c6dc1f02503cf88
Harry Llewelyn <harry@hey.com>
2025-10-24 11:30:36 +0100

    Remove duplicate keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,6 @@ local keymap = vim.keymap
 
 keymap.set('', '<Leader>so', ':so ~/.config/nvim/init.lua<cr>') -- Reload nvim config
 
-keymap.set('n', 'n', 'nzz')
-
 -- Tab mappings
 keymap.set('', '<Leader>tt', ':tabnew<cr>')
 keymap.set('', '<Leader>tc', ':tabclose<cr>')
