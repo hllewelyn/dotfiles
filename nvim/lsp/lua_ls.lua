d3d0e833f515cbf926929dabf718742a93739423
Harry Llewelyn <harry@hey.com>
2025-10-25 14:20:35 +0100

    Disable lua_ls missing fields diagnostics

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -1,3 +1,13 @@
+vim.lsp.config('lua_ls', {
+  settings {
+    Lua = {
+      diagnostics = {
+        disable = { "missing-fields" },
+      },
+    }
+  }
+})
+
 return {
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
